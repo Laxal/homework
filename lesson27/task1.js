b5e3c9cb66a50bfcb495371c02e9e625e2f3ae03
Olexandr Nychyporuk <lax.nich33@gmail.com>
2021-04-20 18:29:34 +0300

    homework27 first task

---
+++ lesson27/task1.js
@@ -0,0 +1,48 @@
+function Product(name, count = 1, bought = false) {
+    this.name = name;
+    this.count = parseFloat(count);
+    this.bought = bought;
+}
+
+let listOfProducts = [
+    new Product('Банан', '4шт.', false),
+    new Product('Чорниці', '1кг', false),
+    new Product('Домашній сир', '0.5кг', true),
+    new Product('Йогурт', '3уп.', true),
+    new Product('Апельсин', '5шт.', false),
+]
+
+function showSortList(arr) {
+    arr.sort((item1, item2) => item1.bought - item2.bought)
+    console.log(arr);
+}
+
+let product1 = new Product('Банан');
+let product2 = new Product('Манго', 5);
+let product3 = new Product('Ананас', '2шт.');
+
+function addProductToList(obj, arr) {
+    let isProduct = arr.find(item => item.name === obj.name);
+
+    if (isProduct !== undefined) {
+        isProduct.count += obj.count;
+    } else arr.push(obj);
+}
+
+function buyProduct(name, arr) {
+    let product = arr.find(item => name === item.name);
+    product.bought = true;
+}
+
+// Тест ================================
+
+
+// addProductToList(product1, listOfProducts);
+// addProductToList(product2, listOfProducts);
+// addProductToList(product3, listOfProducts);
+// buyProduct('Чорниці', listOfProducts);
+// buyProduct('Банан', listOfProducts);
+// buyProduct('Апельсин', listOfProducts);
+// showSortList(listOfProducts);
+
+// console.log(listOfProducts);
